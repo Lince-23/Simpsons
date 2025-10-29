6fec96adf8212047b88683dd4e377dd0a6197ac5
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-29 23:48:18 +0100

    ViewModel and observer is implemented

--- app/src/main/java/com/example/simpsons/features/characters/presentation/SimpsonsListFragment.kt
+++ app/src/main/java/com/example/simpsons/features/characters/presentation/SimpsonsListFragment.kt
@@ -1,11 +1,16 @@
 package com.example.simpsons.features.characters.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.example.simpsons.R
+import com.example.simpsons.core.ApiClient
+import com.example.simpsons.features.characters.data.CharactersDataRepository
+import com.example.simpsons.features.characters.data.remote.api.CharactersApiRemoteDataSource
+import com.example.simpsons.features.characters.domain.GetCharactersListUseCase
 
 
 /**
@@ -14,24 +19,47 @@ import com.example.simpsons.R
  * create an instance of this fragment.
  */
 class SimpsonsListFragment : Fragment() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpObserver()
+        viewModel.loadCharacters()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_simpsons_list, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            SimpsonsListFragment().apply {
+    private val viewModel = SimpsonsListViewModel(
+        GetCharactersListUseCase(
+            CharactersDataRepository(
+                CharactersApiRemoteDataSource(
+                    ApiClient()
+                )
+            )
+        )
+    )
+
+    private fun setUpObserver() {
+        val observer = Observer<SimpsonsListViewModel.UiState> { uiState ->
+            if (uiState.isLoading) {
+
+            } else {
+
             }
+
+            uiState.errorApp?.let {
+                //Todo
+            } ?: {
+                //Todo
+            }
+
+            uiState.charactersList?.let {
+                //Todo mostrar personajes
+            }
+
+        }
     }
 }
