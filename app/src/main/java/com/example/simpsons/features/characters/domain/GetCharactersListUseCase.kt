f9856ed2354d8cc6461ce5c06d8ca4e9ab2f5e12
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-30 00:01:01 +0100

    Added a parameter

--- app/src/main/java/com/example/simpsons/features/characters/domain/GetCharactersListUseCase.kt
+++ app/src/main/java/com/example/simpsons/features/characters/domain/GetCharactersListUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.simpsons.features.characters.domain
 
-class GetCharactersListUseCase(private val charactersRepository: CharactersRepository) {
+class GetCharactersListUseCase(private val charactersRepository: CharactersRepository, private val pageNumber: String) {
     operator fun invoke():Result<List<Character>>{
-        return charactersRepository.getAllCharactersList()
+        return charactersRepository.getAllCharactersList(pageNumber)
     }
 }
