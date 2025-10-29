ad773f72b219e878ae5def493b14c65c4658ef52
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-30 00:08:05 +0100

    Fixed the api service

--- app/src/main/java/com/example/simpsons/features/characters/data/remote/api/CharactersApiService.kt
+++ app/src/main/java/com/example/simpsons/features/characters/data/remote/api/CharactersApiService.kt
@@ -2,8 +2,9 @@ package com.example.simpsons.features.characters.data.remote.api
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CharactersApiService {
-    @GET("characters?page={pageNumber}")
-    fun getAllCharacters(pageNumber: String): Response<ApiResponse>
+    @GET("characters")
+    fun getAllCharacters(@Query("page") page: Int): Response<ApiResponse>
 }
