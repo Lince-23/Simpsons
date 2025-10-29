392e756f59405f18d4f2e1e5e98e12639f77488b
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-29 20:47:46 +0100

    Fragment called at the start of the project

--- app/src/main/java/com/example/simpsons/SimpsonsMainActivity.kt
+++ app/src/main/java/com/example/simpsons/SimpsonsMainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.simpsons
 
+import android.graphics.Insets.add
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.simpsons.features.characters.presentation.SimpsonsListFragment
 
 class SimpsonsMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_simpsons_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<SimpsonsListFragment>(R.id.mainFragment)
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
