6d0839f41556a5c5e247f7d54dddcdc6e27aebbc
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-10-30 12:03:19 +0100

    Order changed for more clarity of code

--- app/src/main/java/com/example/simpsons/SimpsonsMainActivity.kt
+++ app/src/main/java/com/example/simpsons/SimpsonsMainActivity.kt
@@ -16,16 +16,16 @@ class SimpsonsMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_simpsons_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<SimpsonsListFragment>(R.id.mainFragment)
             }
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
