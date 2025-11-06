4516dd905b91bcc8f73f86b1d5de2a678ca99293
Aitor23SR <aitor.sanrom@educa.jcyl.es>
2025-11-06 23:23:18 +0100

    Implemented get character by ID

--- app/src/main/java/com/example/simpsons/features/characters/data/remote/api/CharactersApiService.kt
+++ app/src/main/java/com/example/simpsons/features/characters/data/remote/api/CharactersApiService.kt
@@ -1,11 +1,16 @@
 package com.example.simpsons.features.characters.data.remote.api
 
 import com.example.simpsons.features.characters.data.remote.api.model.ApiResponse
+import com.example.simpsons.features.characters.data.remote.api.model.CharacterDetailsApiModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharactersApiService {
     @GET("characters")
     suspend fun getAllCharacters(@Query("page") page: Int): Response<ApiResponse>
+
+    @GET("characters/{id}")
+    suspend fun getCharacterById(@Path("id") id: String): Response<CharacterDetailsApiModel>
 }
